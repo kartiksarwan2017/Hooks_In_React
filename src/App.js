d68b790b7abfff13bd90be823af33319201d03bd
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-06-07 12:55:45 +0530

    useCallBack Hook - Example 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import Example1 from './components/Example1';
 import Example2 from './components/Example2';
 import Example3 from './components/Example3';
@@ -9,9 +9,14 @@ function App() {
 
   const [show, setShow] = useState(false);
 
-  const fetchData = (pow = 1) => {
+  /* useCallback() hook returns the memoized function and it is getting stored in fetchData. So upon rerendering
+  of the App Component the function fetchData doesn't get created again if the dependency array is empty.
+
+  the function gets created only if the value of the state variable gets changed that is present inside the dependency array of useCallback() hook
+  */
+  const fetchData = useCallback((pow = 1) => {
     return Math.random() * 25 * pow;
-  }
+  }, []);
 
   return ( 
     <>
