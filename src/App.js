81ca7febabfabda267efadf1644bcc1298ac954a
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-06-07 12:41:03 +0530

    useCallBack Hook - Example 1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
 
   const [show, setShow] = useState(false);
 
+  const fetchData = (pow = 1) => {
+    return Math.random() * 25 * pow;
+  }
+
   return ( 
     <>
     <div className="app-container">
@@ -19,7 +23,9 @@ function App() {
       <Example3 />
 
       <h1 style={{margin: "40px 0"}}>UseCallback() Hook Examples</h1>
-      <Boi />
+
+      {/* Reference of the function fetchData is passed as prop in Boi Component */}
+      <Boi fetchData = {fetchData} />
 
       <button onClick={() => setShow((prev) => !prev)}>
         {show ? "Hide" : "Reveal"}
--- src/components/Boi.jsx
+++ src/components/Boi.jsx
@@ -1,11 +1,43 @@
-import React, { memo, useState } from 'react';
+// import React, { memo, useState } from 'react';
 
-const Boi = () => {
+// const Boi = () => {
+
+//   console.log("I am Boi");
+
+//   return (
+//     <div>Boi</div>  
+//   )
+// }
+
+// export default memo(Boi);
+
+/* When we click on the button the show state variable gets
+updated and component App gets rerendered which leads to creation of fetchData() function again.
+
+IF the function gets created again then In Boi Component 
+we have added fetchData method in the dependency array 
+of the useEffect() Hook  so setNum(fetchData(5)) gets called and we get the different value 
+so upon clicking the button in App Component the value of num in the Boi Component gets changed
+
+In order to resolve this we use useCallback() Hook
+*/
+
+import React, { useEffect, useState, memo } from 'react';
+
+const Boi = ({fetchData}) => {
 
   console.log("I am Boi");
+  const [num, setNum] = useState("");
+
+  useEffect(() => {
+    setNum(fetchData(5));
+  }, [fetchData]);
 
   return (
-    <div>Boi</div>  
+    <div>
+      Boi
+      <p>{num}</p>
+    </div>  
   )
 }
 
